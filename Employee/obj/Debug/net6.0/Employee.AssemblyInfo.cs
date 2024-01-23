495ff1f057e313aef2d2ff9abc293794ce2e9e7b
sumit-equitec <sumit.equitec@gmail.com>
2024-01-23 19:11:40 +0530

    apply css

--- Employee/obj/Debug/net6.0/Employee.AssemblyInfo.cs
+++ Employee/obj/Debug/net6.0/Employee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a2b663dee29fab89483ddbf2e7e5dc9632d8ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
